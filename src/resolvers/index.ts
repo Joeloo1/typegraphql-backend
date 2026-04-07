d116fa4903dc725409856aafc405227d1cbeb76a
Joeloo1 <agboolajoel123@gmail.com>
2026-04-07 18:48:04 +0100

    feat: add auth context, login and me resolvers, session types; update resolver index

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,3 +1,5 @@
 import { RegisterResolver } from "./user/register.resolver";
+import { LoginResolver } from "./user/login.resolver";
+import { MeResolver } from "./user/me.resolver";
 
-export const resolvers = [RegisterResolver] as const;
+export const resolvers = [RegisterResolver, LoginResolver, MeResolver] as const;

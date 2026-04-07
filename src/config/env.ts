0e1855a0f379cbe449505008bfb024f5323ec227
Joeloo1 <agboolajoel123@gmail.com>
2026-04-07 18:52:11 +0100

    chore: update project config, environment setup, Redis config, and dependencies

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,4 +4,8 @@ dotenv.config();
 
 export const config = {
   Port: Number(process.env.PORT),
+  NodeEnv: process.env.NODE_ENV || "development",
+  RedisHost: process.env.REDIS_HOST!,
+  CORSOrigin: process.env.CORS_ORIGIN!,
+  SessionSecret: process.env.SESSION_SECRET!,
 };
